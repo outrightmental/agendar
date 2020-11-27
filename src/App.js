ba22e435551c56fdc2ec95399d730c1f29793021
Charney <charneykaye@gmail.com>
2020-11-26 20:50:58 -0800

    Login with Google OAuth 2.0
    Implements #2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
+/* global gapi */
+import React, {Component} from 'react';
 import './App.css';
+import {GOOGLE_CLIENT_ID} from "./config";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isSignedIn: false,
+    }
+
+  }
+
+  componentDidMount() {
+
+    const successCallback = this.onSuccess.bind(this);
+
+    window.gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: GOOGLE_CLIENT_ID,
+      })
+
+      this.auth2.then(() => {
+        console.log('on init');
+        this.setState({
+          isSignedIn: this.auth2.isSignedIn.get(),
+        });
+      });
+    });
+
+    window.gapi.load('signin2', function () {
+      // Method 3: render a sign in button
+      // using this method will show Signed In if the user is already signed in
+      var opts = {
+        width: 200,
+        height: 50,
+        client_id: GOOGLE_CLIENT_ID,
+        onsuccess: successCallback
+      }
+      gapi.signin2.render('loginButton', opts)
+    })
+  }
+
+  onSuccess() {
+    console.log('on success')
+    this.setState({
+      isSignedIn: true,
+      err: null
+    })
+  }
+
+  doLogout() {
+    let self = this;
+    this.auth2.signOut().then(
+      () => {
+        self.setState({
+          isSignedIn: false,
+          err: null
+        })
+        window.location.reload(false);
+      },
+      () => {
+        this.setState({
+          isSignedIn: true,
+          err: "Failed to logout!"
+        });
+      }
+    );
+  }
+
+  getContent() {
+    if (this.state.isSignedIn) {
+      return (
+        <div>
+          <p>hello user, you're signed in </p>
+          <button onClick={() => this.doLogout()}>Logout</button>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <p>You are not signed in. Click here to sign in.</p>
+          <button id="loginButton">Login with Google</button>
+        </div>
+      )
+    }
+  }
+
+  getError() {
+    if (this.state.err) {
+      return (
+        <div>
+          <p className="error">{this.state.err}</p>
+        </div>
+      )
+    } else return "";
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Agendar<sup>&trade;</sup></h1>
+          <h2>Heads-Up Display for being on time.</h2>
+          {this.getContent()}
+          {this.getError()}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const GOOGLE_CLIENT_ID =
+  '307271403290-nirg6c5afjm47qtlloufj0kgrfd05ui7.apps.googleusercontent.com'; // Production
+// '307271403290-nirg6c5afjm47qtlloufj0kgrfd05ui7.apps.googleusercontent.com'; // Development
