c1771c0f81967355da17626ee7df803e57e9d259
Charney <charneykaye@gmail.com>
2020-11-26 23:34:38 -0800

    production keys

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 export const
 
   GOOGLE_API_KEY =
-    // 'AIzaSyDMeOHdQ7sRjWwmK4pXBsL8KPDXYXEyOdw', //Production
-    'AIzaSyDGMNlKWChcdRnm06FaAPl5qSJzM8_9XFY', // Development
+    'AIzaSyDMeOHdQ7sRjWwmK4pXBsL8KPDXYXEyOdw', //Production
+    // 'AIzaSyDGMNlKWChcdRnm06FaAPl5qSJzM8_9XFY', // Development
 
   GOOGLE_CLIENT_ID =
-    // '716546716200-k27gns7n7emjglrn2bec5jui3323r0ua.apps.googleusercontent.com', // Production
-    '307271403290-nirg6c5afjm47qtlloufj0kgrfd05ui7.apps.googleusercontent.com', // Development
+    '716546716200-k27gns7n7emjglrn2bec5jui3323r0ua.apps.googleusercontent.com', // Production
+    // '307271403290-nirg6c5afjm47qtlloufj0kgrfd05ui7.apps.googleusercontent.com', // Development
 
   GOOGLE_SCOPE = [
     "https://www.googleapis.com/auth/calendar.events.readonly",
