fbc8fb4dffa92cab64e9b2346f5ce4f2ff4b7cf4
Charney <charneykaye@gmail.com>
2020-11-27 16:12:25 -0800

    Content is readable by search engines
    Resolves #29
    
    Content displayed on the site should be written on the index.html page in specially tagged elements.
    
    When the React app starts, it will read these special elements to populate its content inside the menu content

--- src/config.js
+++ src/config.js
@@ -22,10 +22,6 @@ export const
     scope: GOOGLE_SCOPE,
   },
 
-  LANG_PRIVACY_PROMISE_1 = <span>We <em>never</em> access your information!</span>,
-
-  LANG_PRIVACY_PROMISE_2 = <span>Your browser will connect directly to Google Calendar<br/> in order display upcoming events in a unique way.</span>,
-
   BEAT_INTERVAL_MILLIS = 1000, // once per second, like a clock
 
   CACHE_INVALIDATE_MILLIS = 1000 * 60 * 60,
